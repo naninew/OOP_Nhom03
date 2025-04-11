1be00c5c0ff0361bf1bc9a484338868e84c296da
vivuong166 <vuongvi221@gmail.com>
2025-04-11 18:04:21 +0700

    xoa

--- Mode.py
+++
@@ -1,9 +0,0 @@
-from nicegui import ui
-def Mode():
-     dark_mode=ui.dark_mode()
-     with ui.row().classes("w-full items-center"):
-             #result=ui.label().classes("mr-auto")
-             with ui.button(icon="menu"):
-                 with ui.menu() as menu:    
-                      ui.menu_item("Light Mode",on_click=dark_mode.disable)
-                      ui.menu_item("Dark Mode",on_click=dark_mode.enable)
